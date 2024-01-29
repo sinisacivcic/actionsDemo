b276510392344b5eaa497bacdc066f1cb472f1c9
sinisa <sinisha.ch@gmail.com>
2024-01-29 09:17:06 +0100

    initial project setup.

---
+++ actionsDemo/actionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  actionsDemoApp.swift
+//  actionsDemo
+//
+//  Created by si on 29.1.24..
+//
+
+import SwiftUI
+
+@main
+struct actionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
